9b50ee916acb3fbed32b4d802ab4049edf1ff06a
Marcel Roozekrans <marcel@roozekrans.nl>
2026-05-06 18:53:29 +0200

    chore(mediator.authorization): scaffold generator project

---
+++ src/ZeroAlloc.Mediator.Authorization.Generator/AuthorizationGenerator.cs
@@ -0,0 +1,10 @@
+namespace ZeroAlloc.Mediator.Authorization.Generator;
+
+[Microsoft.CodeAnalysis.Generator(Microsoft.CodeAnalysis.LanguageNames.CSharp)]
+public sealed class AuthorizationGenerator : Microsoft.CodeAnalysis.IIncrementalGenerator
+{
+    public void Initialize(Microsoft.CodeAnalysis.IncrementalGeneratorInitializationContext context)
+    {
+        // Emission added in Tasks 7-8.
+    }
+}
